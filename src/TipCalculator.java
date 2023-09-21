607a07afb1c6d11f1ffe1ea712a7dc69a5e142f0
BT_2N2_20 <sample@bths.edu>
2023-09-21 13:37:34 -0400

    uploading to GitHub for the first time

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class TipCalculator {
     public static void main(String [] args) {
-    System.out.println("Hello there!");
-    System.out.println("Welcome to my tip calculator!");
-    System.out.println("How many people are in your group?");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello there!");
+        System.out.println("Welcome to my tip calculator!");
+        System.out.println("How many people are in your group?");
+        int people = scan.nextInt();
+        scan.nextLine();
+        System.out.println("What would you like your tip percentage to be?");
+        double percentage = scan.nextDouble();
+        scan.nextLine();
+        double cost = 0;
+        String item;
+        while (cost != -1) {
+            System.out.println("What is an item that you ordered?");
+            item = scan.nextLine();
+            System.out.print
+        }
     }
 }
