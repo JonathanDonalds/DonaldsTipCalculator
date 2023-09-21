54e3c7a7b7f8faba25ca788c5eb0bfba6f624bf9
BT_2N2_20 <sample@bths.edu>
2023-09-21 13:20:56 -0400

    initial commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,7 @@
+public class TipCalculator {
+    public static void main(String [] args) {
+    System.out.println("Hello there!");
+    System.out.println("Welcome to my tip calculator!");
+    System.out.println("How many people are in your group?");
+    }
+}
